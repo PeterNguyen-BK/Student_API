42d33a33d4a7b769bf02af375aa7e38f0dd08da7
peternguyennvt <thuan156109@gmail.com>
2021-07-09 11:48:38 +0700

    update router

--- routes/student.route.js
+++ routes/student.route.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/student.controller');
 
-router.get('/', controller.getStudents);
+router.route('/')
+    .get(controller.getStudents)
+    .post(controller.createStudent);
 
-router.get('/:studentid', controller.getDetailStudent);
-
-router.post('/', controller.createStudent);
-
-router.put('/:studentid', controller.updateStudent);
-
-router.delete('/:studentid', controller.deleteStudent);
+router.route('/:studentid')
+    .get(controller.getDetailStudent)
+    .put(controller.updateStudent)
+    .delete(controller.deleteStudent);
 
 module.exports = router;
+
+// repository, text index
